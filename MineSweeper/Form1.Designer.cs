9c7dd597b8fd2d3f188a42016b726c3ffa50c9e7
Vlad Cojocaru <vladvcojocaru@gmail.com>
2024-03-02 10:47:09 +0200

    added load tiles function

--- MineSweeper/Form1.Designer.cs
+++ MineSweeper/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(84, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(535, 535);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(582, 633);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(18, 20);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(486, 633);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(717, 671);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MineSweeper/Form1.cs
+++ MineSweeper/Form1.cs
@@ -16,5 +16,43 @@ namespace MineSweeper
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {  
+            load_tiles();
+
+        }
+
+        private void load_tiles()
+        {
+            PictureBox[] tile = new PictureBox[100];
+            int x = 0;
+            int y = 0;
+            int cnt = 0;
+
+
+            for(int i = 0; i < 100; i++)
+            {
+                tile[i] = new PictureBox();
+
+                tile[i].Height = 40;
+                tile[i].Width = 40;
+                tile[i].Left = x;
+                tile[i].Top = y;
+
+                tile[i].BorderStyle = BorderStyle.FixedSingle;
+                panel1.Controls.Add(tile[i]);
+
+                x += 40;
+                cnt++;
+
+                if(cnt == 10)
+                {
+                    x = 0;
+                    y += 40;
+                    cnt = 0;
+                }
+            }
+        }
     }
 }
