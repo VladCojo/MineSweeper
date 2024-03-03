c14626d6740ccfcc76b194ca3cf68c3f56fa5f1c
Vlad Cojocaru <vladvcojocaru@gmail.com>
2024-03-03 09:15:55 +0200

    changed images with btns

--- MineSweeper/Form1.cs
+++ MineSweeper/Form1.cs
@@ -13,8 +13,9 @@ namespace MineSweeper
     public partial class Form1 : Form
     {
 
-        private int score = 0;
-        private PictureBox[] tiles = new PictureBox[100];
+        private static int ROW = 10;
+        private static int COL = 10;
+        private Button[,] tiles = new Button[ROW, COL];
         public Form1()
         {
             InitializeComponent();
@@ -28,87 +29,36 @@ namespace MineSweeper
 
         private void load_tiles()
         {
-            PictureBox[] tile = new PictureBox[100];
+            
             int x = 0;
             int y = 0;
-            int cnt = 0;
-            Random rand = new Random();
-
-
-            for(int i = 0; i < 100; i++)
+           
+            for(int i = 0; i < 10; i++)
             {
-                int num = rand.Next(0, 2);
-
-                tiles[i] = new PictureBox();
-
-                tiles[i].Height = 40;
-                tiles[i].Width = 40;
-                tiles[i].Left = x;
-                tiles[i].Top = y;
-                tiles[i].BorderStyle = BorderStyle.FixedSingle;
-                tiles[i].Image = imageList1.Images[2];
-               
-
-                if (num == 0)
+                for (int j = 0; j < 10; j++)
                 {
-                    tiles[i].Tag = "bomb";
+
+                    tiles[i, j] = new Button();
+
+                    tiles[i, j].Height = 40;
+                    tiles[i, j].Width = 40;
+                    tiles[i, j].Left = x;
+                    tiles[i, j].Top = y;
+                    tiles[i, j].Click += new EventHandler(ClickTile);
+
+                    panel1.Controls.Add(tiles[i, j]);
+
+                    x += 40;
+                    
                 }
-                else
-                {
-                    tiles[i].Tag = "ok";
-                }
-
-                tiles[i].SizeMode = PictureBoxSizeMode.StretchImage;
-               
-
-                tiles[i].Click += new EventHandler(ClickTile);
-
-                panel1.Controls.Add(tiles[i]);
-
-                x += 40;
-                cnt++;
-
-                if(cnt == 10)
-                {
-                    x = 0;
-                    y += 40;
-                    cnt = 0;
-                }
+                y += 40;
+                x = 0;
             }
         }
 
         private void ClickTile(object sender, EventArgs e)
         {
-            DialogResult ans;
-            PictureBox tile = (PictureBox)sender;
-           
-            if(tile.Tag.ToString() == "bomb")
-            {
-                tile.Image = imageList1.Images[0];
-                ans = MessageBox.Show("Game Over.\nScore: " + score+"\nPlay Again?", "Game Over", 
-                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-
-                if (ans == DialogResult.Yes)
-                {
-                    score = 0;
-                    lblScore.Text = "0";
-                    foreach(PictureBox it in tiles)
-                    {
-                        panel1.Controls.Remove(it);
-                    }
-                    load_tiles();
-                }
-                else
-                {
-                    Application.Exit();
-                }
-            }
-            else if (tile.Tag.ToString() == "ok")
-            {
-                tile.Image= imageList1.Images[1];
-                score += 5;
-                lblScore.Text = score.ToString();
-            }
+            MessageBox.Show("btn pressed");
         }
     }
 }
